f5b5a9892f8b6b1ae78c22953a5c57eba8d93560
AlexzPurewoko <purwoko908@gmail.com>
2020-10-19 16:41:20 +0700

    stabiling the hero element when resizing by change translate into overlay

--- src/scripts/n_ui/component/hero/HomeHero.ts
+++ src/scripts/n_ui/component/hero/HomeHero.ts
@@ -1,28 +1,13 @@
 import "../../../../styles/n_sass/main/hero-main.sass";
-import { Util } from "../../../n_utils/util";
 
 class HomeHero extends HTMLElement {
 
-    private scaleFinished: boolean = true;
     private isAnimStopped: boolean = true;
     private listImage: JQuery<Element>[] = [];
     private listText: JQuery<Element>[] = [];
     private currentIndex: number = 0;
     private currentTextIndex: number = 0;
 
-    onResize() {
-        //this.style.height = Util.computeHeight(this.querySelector(".hero_img__faded")) + "px";
-        if (this.scaleFinished) {
-            this.scaleFinished = false;
-            setTimeout(() => {
-                const imageElement = this.querySelector(".hero_img__faded");
-                const compHeight = Util.computeHeight(imageElement);
-                const elm = <HTMLElement>this.querySelector("#hero__container");
-                elm.style.height = compHeight + "px";
-                this.scaleFinished = true;
-            }, 150);
-        } else return;
-    }
 
     pauseAnim() {
         this.isAnimStopped = true;
@@ -37,49 +22,45 @@ class HomeHero extends HTMLElement {
 
     render() {
         this.innerHTML = `
-        <div id="hero__container">
-            <div 
-                class='hero__image' 
-                id='image__hero_main'>
-                
+
+            <div class="hero__container">
                 <img 
                     id='hero-image_1'
 
-                    class='hero_img__faded' 
+                    class='hero__img' 
                     src='/images/heros/hero-image_1.jpg' 
                     alt='Our master chef image'></img>
 
                 <img 
                     id='hero-image_2'
                     style='display: none;'
-                    class='hero_img__faded transparent' 
+                    class='hero__img transparent' 
                     src='/images/heros/hero-image_2.jpg' 
                     alt='A menu image'></img>
 
                 <img 
                     id='hero-image_3'
                     style="display: none;"
-                    class='hero_img__faded transparent' 
+                    class='hero__img transparent' 
                     src='/images/heros/hero-image_3.jpg' 
                     alt='Ingredients image'></img>
+                <div 
+                    class='hero__text' 
+                    id='explain__text_hero'>
+                    <article id='text-hero_1'>
+                        <h1 tabIndex="0" id='text_container_1'>Chef</p>
+                        <h2 tabIndex="0" id='text_container_2'>We have a most talented chef in my restaurant's group</p>
+                    </article>
+                    <article id='text-hero_2' style="display: none;">
+                        <h1 tabIndex="0" id='text_container_1'>Food</p>
+                        <h2 tabIndex="0" id='text_container_2'>We serve many of good food beside's you want</p>
+                    </article>
+                    <article id='text-hero_3' style="display: none;">
+                        <h1 tabIndex="0" id='text_container_1' >Ingredients</p>
+                        <h2 tabIndex="0" id='text_container_2'>We selecting many good ingredients for making a taste food</p>
+                    </article>
+                </div>
             </div>
-            <div 
-                class='hero__text' 
-                id='explain__text_hero'>
-                <article id='text-hero_1'>
-                    <h1 tabIndex="0" id='text_container_1'>Chef</p>
-                    <h2 tabIndex="0" id='text_container_2'>We have a most talented chef in my restaurant's group</p>
-                </article>
-                <article id='text-hero_2' style="display: none;">
-                    <h1 tabIndex="0" id='text_container_1'>Food</p>
-                    <h2 tabIndex="0" id='text_container_2'>We serve many of good food beside's you want</p>
-                </article>
-                <article id='text-hero_3' style="display: none;">
-                    <h1 tabIndex="0" id='text_container_1' >Ingredients</p>
-                    <h2 tabIndex="0" id='text_container_2'>We selecting many good ingredients for making a taste food</p>
-                </article>
-            </div>
-        </div>
         `;
         this.listImage = [
             $(this.querySelector("#hero-image_1")),
@@ -92,20 +73,19 @@ class HomeHero extends HTMLElement {
             $(this.querySelector("#text-hero_2")),
             $(this.querySelector("#text-hero_3"))
         ]
-        this.onResize();
     }
 
     private anim() {
         if (this.isAnimStopped) return;
         setTimeout(() => {
             if (this.isAnimStopped) return;
-            this.listImage[this.currentIndex++].slideUp('slow');
+            this.listImage[this.currentIndex++].slideUp();
 
             if (this.currentIndex >= this.listImage.length) this.currentIndex = 0;
-            this.listImage[this.currentIndex].slideDown('slow', () => this.anim());
-            this.listText[this.currentTextIndex++].fadeOut('slow', () => {
+            this.listImage[this.currentIndex].slideDown(() => this.anim());
+            this.listText[this.currentTextIndex++].fadeOut(() => {
                 if (this.currentTextIndex >= this.listText.length) this.currentTextIndex = 0;
-                this.listText[this.currentTextIndex].fadeIn('slow');
+                this.listText[this.currentTextIndex].fadeIn();
             })
         }, 5000);
     }

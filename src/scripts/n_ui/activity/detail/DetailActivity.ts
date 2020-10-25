0d44cb6ce29ba617d1aa605d276e735911ad2947
AlexzPurewoko <purwoko908@gmail.com>
2020-10-25 13:51:37 +0700

    init composing detail activity

--- src/scripts/n_ui/activity/detail/DetailActivity.ts
+++ src/scripts/n_ui/activity/detail/DetailActivity.ts
@@ -1,20 +1,76 @@
 import { Button } from "../../../n_utils/element_types";
+import DetailSummary from "../../component/detail/DetailSummary";
+import DetailActivityHeader from "../../component/header/DetailActivityHeader";
+import RoundedImages from "../../component/image/RoundedImages";
 import BaseActivity from "../base/BaseActivity";
+import { revData } from "./_cobaData";
+import * as utils from "./_utils";
+import "../../../../styles/n_sass/detail/detail-activity.sass"
+import ConsumerReview from "../../component/review/ConsumerReview";
+import { AddReviewCallback } from "../../../n_utils/callbacks/AddReviewCallback";
+import { IPostReview } from "../../../n_logic/api/data/review/IPostReview";
+import NavItemCallback from "../../../n_utils/callbacks/NavItemCallback";
+import MainObjStore from "../../../n_logic/db/MainObjStore";
+import { update } from "lodash";
+import { IDetailRestaurantItem } from "../../../n_logic/api/data/detail/IDetailRestaurantItem";
+import ApiCallbacks from "../../../n_logic/api/modules/base/ApiCallbacks";
+import { IAllResponse } from "../../../n_logic/api/allresponse/IAllResponse";
+import BaseApi from "../../../n_logic/api/modules/base/BaseApi";
+import GetRestaurantDetail from "../../../n_logic/api/modules/detail/GetRestaurantDetail";
+import { IRestaurantDetailResponse } from "../../../n_logic/api/data/detail/IRestaurantDetailResponse";
+import PostReview from "../../../n_logic/api/modules/reviewResult/PostReview";
+import { IResultReview } from "../../../n_logic/api/data/review/IResultReview";
 
-class DetailActivity extends BaseActivity {
+class DetailActivity extends BaseActivity implements ApiCallbacks{
 
-    private buttonBack: Button = null;
+    private _header: DetailActivityHeader = null;
+    private _roundedImages: RoundedImages = null;
+    private _detailSummary: DetailSummary = null;
+    private _reviewList: ConsumerReview = null;
+    private _detailDescription: HTMLElement = null;
+    private _addReviewLayout: HTMLElement = null;
+
+    private _addReviewCb: AddReviewCallback = (dataContract: IPostReview) => {
+        this.addReviewCbImpl(dataContract);
+    }
+
+    private _navItemCv: NavItemCallback = (elmRef: HTMLElement, anchorRef: string) => {
+        this.navItemCb(elmRef, anchorRef);
+    }
+
+    private _isHeaderSticky: boolean = false
+    private _isFavorite = false;
+    private _reviewPostApi = false;
+    private _restaurantData : IDetailRestaurantItem = null;
+
+    private _apiCall: GetRestaurantDetail = null;
+
+
     onCreated(params: any[]): void {
         super.onCreated(params);
-        this.innerHTML = `
-            <p>${params[0]}</p>
-            <button>Back click</button>
-        `;
+        this.innerHTML = this.render();
+        console.log(params);
+        
+        this._header = this.querySelector("detail-header");
+        this._roundedImages = this.querySelector("rounded-images");
+        this._detailSummary = this.querySelector("detail-summary");
+        this._detailDescription = this.querySelector(".description");
+        this._reviewList = this.querySelector(".reviews > .list-review");
+        this._addReviewLayout = this.querySelector(".add-reviews");
 
-        this.buttonBack = this.querySelector("button");
-        this.buttonBack.addEventListener("click", (e: Event) => {
-            this._application.activityBack();
-        })
+        this._header.callback = this._navItemCv;
+        this._header.render();
+
+        const data = revData;
+        
+        this._header.isDisabled = true
+        if(params[1] !== undefined && params[1] === "fromFavorite"){
+            this.loadFromFavorite(params[0]);
+        } else {
+            this.loadFromApi(params[0]);
+        }
+        
+        this._restaurantData = data.restaurant;
     }
     onPaused(): void {
         
@@ -29,7 +85,237 @@ class DetailActivity extends BaseActivity {
         
     }
     onScrollEvent(event: Event): void {
-        
+        this.checkAndStickyHeader();
+    }
+
+    private render(): string {
+        return `
+            <header>
+                <detail-header></detail-header>
+            </header>
+
+            <main>
+                <article class="summary_info">
+                    <section class="image-poster">
+                        <rounded-images></rounded-images>
+                    </section>
+
+                    <section class="summary">
+                        <detail-summary></detail-summary>
+                    </section>
+                </article>
+
+                
+                <article class="description">
+                    <section class="titles">
+                        <h1 class="title-section">Description</h1>
+                        <spacer-line class="title-section-line"></spacer-line>
+                    </section>
+
+                    <section class="content">
+                        <p></p>
+                    </section>
+                </article>
+
+                
+                <article class="menus">
+                    <section class="food-menu">
+                        <h1 class="title-section">Food Menu's</h1>
+                        <spacer-line class="title-section-line"></spacer-line>
+                        <list-badge></list-badge>
+                    </section>
+
+                    <section class="drink-menu">
+                        <h1 class="title-section">Drink's Menu</h1>
+                        <spacer-line class="title-section-line"></spacer-line>
+                        <list-badge></list-badge>
+                    </section>
+                </article>
+                
+
+                <article class="reviews">
+                    <section class="list-review">
+                        <!-- generates dynamically -->
+                        <h1 class="title-section">All Reviews</h1>
+                        <spacer-line class="title-section-line"></spacer-line>
+                        <div class="list"></div>
+                    </section>
+
+                    <section class="add-reviews">
+                        <h1 class="title-section">Add Review Here</h1>
+                        <spacer-line class="title-section-line"></spacer-line>
+                        <compose-review></compose-review>
+                    </section>
+                </article>
+            </main>
+
+            <footer>
+
+            </footer>
+        `;
+    }
+
+    private addReviewCbImpl(dataContract: IPostReview){
+        console.log(dataContract);
+        const api = new PostReview(dataContract);
+        api.callbacks = this;
+        api.startLoad();
+        this._reviewPostApi = true;
+    }
+
+    onFinished(data: IAllResponse) {
+
+        if(!data.isSuccess){
+            // display failed here
+
+            return;
+        }
+
+        if(this._reviewPostApi){
+            const response = <IResultReview> data.response;
+            utils.editReviewItems(response, this._reviewList);
+        } else {
+            const response = <IRestaurantDetailResponse> data.response;
+            this._restaurantData = response.restaurant;
+            this.composeUI(true);
+        }
+    };
+
+    onLoad() {
+
+    }
+
+    private loadFromFavorite(id: string) {
+        this.database.getByKey(MainObjStore.MAIN_DATABASE, id)
+            .then((data: IDetailRestaurantItem) => {
+                this._restaurantData = data;
+                this._header.isDisabled = false
+                this.composeUI(true);
+            })
+            .catch((e: any) => {
+                console.error(e)
+                this._restaurantData = null;
+                this.composeUI(false, e);
+            })
+    }
+
+    private loadFromApi(id: string) {
+        const data = revData;
+
+        if(this._apiCall ){
+            this._apiCall.callbacks = null;
+            this._apiCall = null;
+        }
+
+        this._apiCall = new GetRestaurantDetail()
+        this._apiCall.id = id;
+        this._apiCall.callbacks = this;
+        this._apiCall.startLoad();
+
+    }
+
+    private composeUI(isSuccess: boolean, messages?: any) {
+        if(!isSuccess){
+
+            // display the error message here
+            return;
+        }
+        // init roundedImages
+        utils.applyRoundedImages(this._restaurantData, this._roundedImages);
+
+        // init detailSummary
+        utils.applySummaryDetails(this._restaurantData, this._detailSummary);
+
+        // init detailDescription
+        utils.applyDescriptionDetails(this._restaurantData, this._detailDescription);
+
+        // init menus Section
+        utils.generateFoodAndDrinksSection(this._restaurantData, this.querySelector(".menus"));
+
+        // init reviews section
+        utils.applyListReviews(this._restaurantData, this._reviewList);
+
+        // init add reviews section 
+        utils.addLayoutComposeReview(this._restaurantData, this._addReviewCb, this._addReviewLayout);
+
+        this.checkFavorite(this._restaurantData.id);
+    }
+
+    private checkAndStickyHeader() {
+        const offTop = window.pageYOffset;
+        const header = this._header.parentElement;
+        const offTopMin = $(header).height();
+        if(offTop > offTopMin && !this._isHeaderSticky){
+            header.classList.add("sticky-top");
+            this._isHeaderSticky = true;
+        } else if(offTop <= offTopMin && this._isHeaderSticky) {
+            header.classList.remove("sticky-top");
+            this._isHeaderSticky = false;
+        }
+    }
+
+    private navItemCb(elmRef: HTMLElement, hrefAnchor: string) {
+        switch(hrefAnchor){
+            case "favorite": {
+                this.onFavClick();
+                break;
+            }
+            case "back": {
+                this.application.activityBack();
+            }
+        }
+    }
+
+    private onFavClick(){
+        this._header.isDisabled = true
+        if(this._isFavorite){
+            this.database.deleteData(MainObjStore.MAIN_DATABASE, this._restaurantData.id)
+                .then((isSuccess) => {
+                    if(isSuccess) {
+                        this._isFavorite = false
+                        this._header.isDisabled = false
+                        this.updateFavoriteBtn();
+                    }
+                })
+                .catch((e: any) => {
+                    console.error(e);
+                    this._header.isDisabled = false
+                })
+        } else {
+            this.database.addData(MainObjStore.MAIN_DATABASE, this._restaurantData)
+                .then((isSuccess: boolean) => {
+                    if(isSuccess){
+                        this._isFavorite = true
+                        this._header.isDisabled = false
+                        this.updateFavoriteBtn();
+                    }
+                })
+                .catch((e: any) => {
+                    console.error(e);
+                    this._header.isDisabled = false
+                })
+        }
+    }
+
+    private checkFavorite(keyId: string){
+        this._header.isDisabled = true
+        this.database.anyData(MainObjStore.MAIN_DATABASE, keyId)
+            .then((isFav: boolean) => {
+                this._isFavorite = isFav;
+                this.updateFavoriteBtn();
+                this._header.isDisabled = false
+                
+            })
+            .catch((e: any) => {
+                this._isFavorite = false;
+                this.updateFavoriteBtn();
+                this._header.isDisabled = false
+                console.error(e);
+            })
+    }
+
+    private updateFavoriteBtn() {
+        this._header.isFavorite = this._isFavorite;
     }
     
 }
---
+++ src/scripts/n_ui/activity/detail/_utils.ts
@@ -0,0 +1,110 @@
+import { create } from "lodash";
+import { IConsumerReview } from "../../../n_logic/api/data/detail/IConsumerReview";
+import { IDetailRestaurantItem } from "../../../n_logic/api/data/detail/IDetailRestaurantItem";
+import { ISingleName } from "../../../n_logic/api/data/detail/ISingleName";
+import { IResultReview } from "../../../n_logic/api/data/review/IResultReview";
+import { AddReviewCallback } from "../../../n_utils/callbacks/AddReviewCallback";
+import Badges, { BadgeOptions } from "../../component/badge/Badges";
+import DetailSummary from "../../component/detail/DetailSummary";
+import RoundedImages, { RImageAttrs } from "../../component/image/RoundedImages";
+import AddReview from "../../component/review/AddReview";
+import ConsumerReview from "../../component/review/ConsumerReview";
+import SpacerLine, { SpacerAttrs } from "../../component/spacer/SpacerLine";
+
+const spacerAttrs : SpacerAttrs = {
+    color: "#d76700",
+    style: "horizontal",
+    width: 50,
+    height: 6
+};
+
+export const applyRoundedImages = (iItem: IDetailRestaurantItem, target: RoundedImages)=>{
+    const rImageAttrs : RImageAttrs = {
+        src: iItem.pictureLocation,
+        alt: `Poster of ${iItem.name}`,
+        css: {
+            "width" : "330px"
+        }
+    }
+
+    target.properties = rImageAttrs;
+    target.render();
+}
+
+export const applySummaryDetails = (iItem: IDetailRestaurantItem, target: DetailSummary) => {
+    target.data = iItem;
+    target.render();
+}
+
+export const applyDescriptionDetails = (iItem: IDetailRestaurantItem, target: HTMLElement) => {
+    const spacerLineElement : SpacerLine = target.querySelector("spacer-line");
+    const paragraphText : HTMLElement = target.querySelector(".content > p");
+
+    spacerLineElement.attrs = spacerAttrs;
+
+    paragraphText.innerText = iItem.description;
+}
+
+const generateMenus = (iNames: ISingleName[], badgeElement: Badges) => {
+    const options: BadgeOptions = {
+        color: "red",
+        borderColor: "red",
+        bgColor: "white"
+    }
+
+    badgeElement.options = options;
+    badgeElement.item = iNames;
+    badgeElement.render();
+}
+
+export const generateFoodAndDrinksSection = (iItem: IDetailRestaurantItem, target: HTMLElement) => {
+    const foodSection : Badges = target.querySelector(".food-menu > list-badge");
+    const drinkSection : Badges = target.querySelector(".drink-menu > list-badge");
+
+    // spacer line
+    const spacerLineFood: SpacerLine = target.querySelector(".food-menu > spacer-line");
+    const spacerLineDrinks: SpacerLine = target.querySelector(".drink-menu > spacer-line");
+    
+    generateMenus(iItem.menus.foods, foodSection);
+    generateMenus(iItem.menus.drinks, drinkSection);
+
+    spacerLineFood.attrs = spacerAttrs;
+    spacerLineDrinks.attrs = spacerAttrs;
+}
+
+
+export const applyListReviews = (iItem: IDetailRestaurantItem, target: HTMLElement) => {
+    const spacerLine : SpacerLine = target.querySelector("spacer-line");
+    console.log(target);
+    const divListContainer: HTMLElement = target.querySelector(".list");
+
+    spacerLine.attrs = spacerAttrs;
+    divListContainer.innerHTML = '';
+    iItem.consumerReviews.forEach((itemReview: IConsumerReview) => {
+        const createElm = <ConsumerReview> document.createElement("review-item-consumer");
+        createElm.data = itemReview;
+        divListContainer.append(createElm);
+    });
+}
+
+export const addLayoutComposeReview = (iItem: IDetailRestaurantItem, cb: AddReviewCallback, target: HTMLElement) => {
+    const spacerLine: SpacerLine = target.querySelector("spacer-line");
+    const composeReview: AddReview = target.querySelector("compose-review");
+    
+    spacerLine.attrs = spacerAttrs;
+    composeReview.restaurantId = iItem.id;
+    composeReview.onSubmit = cb;
+    composeReview.render();
+} 
+
+export const editReviewItems = (data: IResultReview, target: HTMLElement) => {
+    const divListContainer: HTMLElement = target.querySelector(".list");
+    
+    divListContainer.innerHTML = '';
+    console.log(divListContainer);
+    data.customerReviews.forEach((itemReview: IConsumerReview) => {
+        const createElm = <ConsumerReview> document.createElement("review-item-consumer");
+        createElm.data = itemReview;
+        divListContainer.append(createElm);
+    });
+}

bb662cd12e1e18969c97ac576c943fb55e20cd52
AlexzPurewoko <purwoko908@gmail.com>
2020-10-26 00:37:09 +0700

    installing workbox, pwa manifest and configure

--- webpack.common.js
+++ webpack.common.js
@@ -5,9 +5,14 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
+const webpackPwaManifest = require("webpack-pwa-manifest");
+const workboxPlugin = require("workbox-webpack-plugin");
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src/scripts/n_index.ts'),
+  entry: {
+    main: path.resolve(__dirname, 'src/scripts/n_index.ts'),
+    actSw: path.resolve(__dirname, "src/scripts/n_sw.ts")
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'scripts/[name]-[hash].app.js',
@@ -105,5 +110,38 @@ module.exports = {
         }
       ],
     }),
+
+    new webpackPwaManifest({
+      name: "Favorite Restaurant",
+      short_name: "FavResto",
+      description: "Deliver a best Restaurant information for you!",
+      background_color: "#fff",
+      theme_color: "#d76700",
+      crossorigin: "use-credentials",
+      display: "standalone",
+      start_url: "/index.html",
+      scope: "/",
+      orientation: "any",
+      icons: [
+        {
+          src: path.resolve("src/public/images/icons/icon.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          purpose: "maskable",
+          destination: "images/icons",
+          ios:false
+        },
+        {
+          src: path.resolve("src/public/images/icons/icon.png"),
+          size: 192,
+          destination: "images/icons/ios",
+          ios: true
+        }
+      ]
+    }),
+
+    new workboxPlugin.InjectManifest({
+      swSrc: "./src/template-sw.js",
+      swDest: "service-worker.js"
+    })
   ],
 };

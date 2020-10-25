9f21067d084feac55a8ab46a582d1ff4a08f82e8
AlexzPurewoko <purwoko908@gmail.com>
2020-10-25 15:40:37 +0700

    adding support https

--- webpack.dev.js
+++ webpack.dev.js
@@ -10,7 +10,8 @@ module.exports = merge(common, {
     host: '0.0.0.0',
     port: 8080,
     disableHostCheck: true,
-    open: true
+    open: true,
+    https: true
   },
   module: {
     rules: [

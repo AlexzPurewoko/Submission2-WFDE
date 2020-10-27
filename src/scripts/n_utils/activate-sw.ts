cd84897369b438acfac3349811e6fd9bae033fa8
AlexzPurewoko <purwoko908@gmail.com>
2020-10-27 14:36:31 +0700

    adding feature register service worker separating from main's code

--- src/scripts/n_utils/activate-sw.ts
+++ src/scripts/n_utils/activate-sw.ts
@@ -1,16 +1,19 @@
 export const activateServiceWorker = (activate : boolean) : void => {
     if(!activate) return;
 
+    const listener = () => {
+        navigator.serviceWorker.register("/service-worker.js")
+            .then(() => {
+                console.log("Service Worker Successfully installed!");
+                window.removeEventListener("load", listener);
+            })
+            .catch((e: any) => {
+                console.error("Cannot install Service Worker: ", e);
+                window.removeEventListener("load", listener);
+            });
+    }
     if("serviceWorker" in navigator){
-        window.addEventListener("load", () => {
-            navigator.serviceWorker.register("/service-worker.js")
-                .then(() => {
-                    console.log("Service Worker Successfully installed!");
-                })
-                .catch((e: any) => {
-                    console.error("Cannot install Service Worker: ", e);
-                })
-        })
+        window.addEventListener("load", listener);
     } else {
         console.log("Your browser doesn't support Service Worker :(");
     }
